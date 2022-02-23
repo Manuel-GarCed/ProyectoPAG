04f4c2ebc5217feed3d2ab83920d056b59bc0248
jdquito <j0nathanquito@hotmail.com>
2022-02-22 22:00:34 -0500

    Interfaz y Controlador de los heroes

---
+++ src/controller/PaginaHeroesController.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author carlo
+ */
+public class PaginaHeroesController implements Initializable {
+    
+    @FXML
+    private VBox VboxHeroes;
+    @FXML
+    private Pane PaneBusqueda;
+    @FXML
+    private Label labelHERO;
+    @FXML
+    private TextField TextFBusc;
+    @FXML
+    private Button BtnBuscar;
+    @FXML
+    private HBox HboxHeroes;
+    @FXML
+    private TilePane TilePHeroes;
+    @FXML
+    private ImageView FlechaVerde;
+    @FXML
+    private ImageView Batman;
+    @FXML
+    private ImageView Superman;
+    @FXML
+    private ImageView Spiderman;
+    @FXML
+    private ImageView Iroman;
+    @FXML
+    private ImageView CapitanAmerica;
+    @FXML
+    private ImageView Deadpool;
+    @FXML
+    private ImageView Constantine;
+    @FXML
+    private ImageView ViudaNegra;
+    @FXML
+    private ScrollPane ScroballPag;
+    @FXML
+    private ImageView FLASH;
+    @FXML
+    private ImageView BRUJA_ESCARLATA;
+    @FXML
+    private Button btneditar;
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
